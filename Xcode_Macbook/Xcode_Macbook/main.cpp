adaafe34906af44c605d18c4f7d1b9176bbe433b
Eric Tseng <eric830303@gmail.com>
2017-12-16 22:31:11 +0800

    no message

---
+++ Xcode_Macbook/Xcode_Macbook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode_Macbook
+//
+//  Created by TienHungTseng on 2017/12/16.
+//  Copyright © 2017年 Eric Tseng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
